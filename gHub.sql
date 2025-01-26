4d003f6fa15aee5283536f01707e9d05cdb91f39
Geya Sahithi <73871382+Geya-Sahithi@users.noreply.github.com>
2025-01-26 21:07:45 +0530

    Create gHub.sql

---
+++ gHub.sql
@@ -0,0 +1,9 @@
+SELECT
+   slug AS gh_slug,
+   CONCAT((JSON_VALUE(response, '$.today_availability_by_catalog.STANDARD_DELIVERY[0].from')),'-',JSON_VALUE(response, '$.today_availability_by_catalog.STANDARD_DELIVERY[0].to')) as gh_business_hours,
+   STRUCT(
+     b_name AS b_name,
+     vb_name AS vb_name
+   ) AS restaurant_info 
+ FROM
+   arboreal-vision-339901.take_home_v2.virtual_kitchen_grubhub_hours;
