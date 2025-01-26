dd9dbec5410e0e2cedc616e7bb908059ad0e63f3
Geya Sahithi <73871382+Geya-Sahithi@users.noreply.github.com>
2025-01-26 21:07:03 +0530

    Create uberEats.sql

---
+++ uberEats.sql
@@ -0,0 +1,9 @@
+SELECT
+   slug AS ue_slug,
+   CONCAT(JSON_VALUE(response, '$.data.menus."26bd579e-5664-4f0a-8465-2f5eb5fbe705".sections[0].regularHours[0].startTime'),' - ',JSON_VALUE(response, '$.data.menus."26bd579e-5664-4f0a-8465-2f5eb5fbe705".sections[0].regularHours[0].endTime')) as ue_business_hours,
+   STRUCT(
+     b_name AS b_name,
+     vb_name AS vb_name
+   ) AS restaurant_info
+ FROM
+   arboreal-vision-339901.take_home_v2.virtual_kitchen_ubereats_hours;
